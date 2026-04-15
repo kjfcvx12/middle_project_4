c700952cfee975e13110e41c98f347eded0544fd
kjfcvx12 <kjfcvx12@gmail.com>
2026-04-15 16:56:56 +0900

    04/15 edit2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,36 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const App = () => {
+  const navStyle = {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    height: '60px',
+    backgroundColor: '#fff',
+    borderTop: '1px solid #ccc',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+
   return (
     <div>
-      <Link><button>로그인</button></Link>
-      <Link><button>회원가입</button></Link>
+      <p>home</p>
+
+      
+      <nav className='bottom-nav' style={navStyle}>
+        <table>
+        <tr>
+          <td>home</td>
+          <td>헬스장</td>
+          <td>루틴</td>
+          <td>기록</td>
+          <td>커뮤니티</td>
+          <td>프로필</td>
+        </tr>
+      </table>
+      </nav>
     </div>
   );
 };
