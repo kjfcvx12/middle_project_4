6604db315eb2dd81e364e8eb01dfea6d5ffbf329
enkyong <diw119dks@naver.com>
2026-04-28 20:37:29 +0900

    api + boardCreate & Detail

--- frontend/src/Components/board/Board.jsx
+++ frontend/src/Components/board/Board.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { createBoard } from "../../api/board";
+import BoardDetail from "./BoardDetail";
 
 const Board = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const mode = searchParams.get("mode");
+
   const [boards, set_boards] = useState([]);
   const [page, set_page] = useState(1);
   const [size] = useState(10);
   const [total_count, set_total_count] = useState(0);
+  const [b_content, set_b_content] = useState("");
+
   const total_pages = Math.ceil(total_count / size);
 
   useEffect(() => {
+    if (mode === "create") return;
+
     const fetch_boards = async () => {
       try {
         const response = await fetch(
@@ -25,17 +35,63 @@ const Board = () => {
     };
 
     fetch_boards();
-  }, [page, size]);
+  }, [page, size, mode]);
+
+  const handle_submit = async (e) => {
+    e.preventDefault();
+
+    if (!b_content.trim()) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+
+    try {
+      await createBoard(1, {
+        b_content,
+      });
+
+      alert("게시글 작성 완료");
+      navigate("/board");
+    } catch (error) {
+      console.error("게시글 작성 실패:", error.response?.data || error.message);
+      alert("게시글 작성 실패");
+    }
+  };
+
+  if (mode === "create") {
+    return (
+      <div style={{ paddingBottom: "80px" }}>
+        <h1>글쓰기</h1>
+
+        <form onSubmit={handle_submit}>
+          <textarea
+            value={b_content}
+            onChange={(e) => set_b_content(e.target.value)}
+            placeholder="내용을 입력하세요"
+            rows={10}
+            style={{ width: "100%" }}
+          />
+
+          <button type="submit">등록</button>
+        </form>
+      </div>
+    );
+  }
+  if (mode === "detail") {
+    return <BoardDetail />;
+  }
 
   return (
     <div style={{ paddingBottom: "80px" }}>
       <h1>게시판</h1>
 
-      <Link to="/boards/create">글쓰기</Link>
+      <Link to="/board?mode=create">글쓰기</Link>
 
       {boards.map((board) => (
         <div key={board.b_id}>
-          <Link to={`/board/${board.b_id}`}>게시글 번호 {board.b_id}</Link>
+          <Link to={`/board?mode=detail&id=${board.b_id}`}>
+            게시글 번호 {board.b_id}
+          </Link>
           <p>{board.b_content}</p>
         </div>
       ))}
