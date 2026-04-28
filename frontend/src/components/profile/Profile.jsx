26194d4f486f3dc5c4aeae9fddf145e6f227efc2
강민구 <kjfcvx12@gmail.com>
2026-04-28 11:26:50 +0900

    Update Profile.jsx

--- frontend/src/components/profile/Profile.jsx
+++ frontend/src/components/profile/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useAuth } from '../AuthContext';
 
 const Profile = () => {
+    const {logout} =useAuth();
     return (
         <div>
-            
+            <button onClick={logout}>로그아웃</button>
         </div>
     );
 };
