5ee46ce8231597f155a81be3a4d969311d57076c
enkyong <diw119dks@naver.com>
2026-05-06 17:29:04 +0900

    merge main

--- frontend/src/Components/home/Login.jsx
+++ frontend/src/Components/home/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../AuthContext";
+import fitChickLogo from '../../image/FitChick.png';
 
 const Login = () => {
   const { login, logout, signup } = useAuth();
